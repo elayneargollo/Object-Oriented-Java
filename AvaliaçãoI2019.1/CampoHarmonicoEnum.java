f8f5634427673c3878977b09284c8d6f18fbff1e
Elayne Argollo <elayne.natalia@outlook>
2019-12-07 19:26:31 -0300

    Playlist

---
+++ AvaliaçãoI2019.1/CampoHarmonicoEnum.java
@@ -0,0 +1,15 @@
+package Entendis;
+public enum CampoHarmonicoEnum {
+	C("Do"), D("Re"), E("MiMaior"), F("FaMaior"), G("SolMaior"), A("LaMaior"), B("SiMaior");
+	
+	private String faixa;
+	
+	CampoHarmonicoEnum(String faixa){
+		this.faixa = faixa;
+	}
+	
+	public String getFaixa() {
+		return faixa;
+	}	
+	
+}
